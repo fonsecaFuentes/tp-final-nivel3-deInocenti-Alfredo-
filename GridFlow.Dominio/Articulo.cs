fc10ec30b23b7525991b00c5e2c7d90f1e5ba20c
Alfredo de Inocenti <alfreddinocenti@gmail.com>
2026-03-21 07:28:30 -0300

    Agrego entidades del dominio: Usuario, Articulo, Marca, Categoria y Favorito

---
+++ GridFlow.Dominio/Articulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridFlow.Dominio
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenUrl { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        private decimal? precio;
+        public decimal? Precio
+        {
+            get { return precio.HasValue ? Math.Round(precio.Value, 2) : (decimal?)null; }
+            set { precio = value; }
+        }
+    }
+}
---
+++ GridFlow.Dominio/Favorito.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridFlow.Dominio
+{
+    public class Favorito
+    {
+        public int Id { get; set; }
+        public Usuario Usuario { get; set; }
+        public Articulo Articulo { get; set; }
+    }
+}
